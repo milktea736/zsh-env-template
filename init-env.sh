63178cb8cc5fba5ba135d58d406ee81e7d31678e
Ivan Liu <milktea736@gmail.com>
2021-06-10 14:55:48 +0800

    update install flow for ubuntu

--- init-env.sh
+++ init-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BASEDIR=$(dirname "$0")
+BASEDIR=$(cd $(dirname "$0") && pwd)
 
 function common_install() {
     # download antigen
@@ -19,12 +19,27 @@ function linux_install() {
     # download pyenv
     git clone https://github.com/pyenv/pyenv.git $BASEDIR/pyenv
 
+    # create soft link for bat
+    mkdir -p $BASEDIR/bat/bin
+    ln -s $(which batcat) $BASEDIR/bat/bin/bat
+
+    # install fzf
+    git clone --depth 1 https://github.com/junegunn/fzf.git $BASEDIR/fzf
+    $BASEDIR/fzf/install
+
     # install autojump
     git clone git://github.com/wting/autojump.git $BASEDIR/autojump
     cd $BASEDIR/autojump
-    ./install.py
+    python3 ./install.py
 
-    echo "please add the lines above to the top of zshrc"
+    echo "=== please add the lines above to the top of cd $BASEDIR/zshrc ==="
+
+    # install tmux.conf
+    git clone https://github.com/gpakosz/.tmux.git $BASEDIR/tmux.conf
+    echo "=== Create a symbolic link for tmux.conf ==="
+    echo "=== Please follow the instuctions below ==="
+    echo "ln -s -f $BASEDIR/tmux.conf/.tmux.conf ~/.tmux.conf"
+    echo "cp $BASEDIR/tmux.conf/.tmux.conf.local ~/.tmux.conf.local"
 }
 
 function install_requires(){
