7f9ac6170f8386cc68e5ab13819b51bd5f8f111b
Ivan Liu <milktea736@gmail.com>
2023-07-08 17:15:03 +0800

    update pfc alias to function add jabba

--- host-env.sh
+++ host-env.sh
@@ -18,21 +18,25 @@ export PATH=$PATH:$GOBIN
 export GPG_TTY=$(tty)
 
 ### === export fzf configs ===
-export FZF_DEFAULT_OPTS="--preview-window 'right:57%' --preview 'bat --color always --style=numbers --line-range :300 {}' \
---bind 'ctrl-y:preview-up,ctrl-e:preview-down,\
-ctrl-b:preview-page-up,ctrl-f:preview-page-down,\
-ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,\
-shift-up:preview-top,shift-down:preview-bottom,\
-alt-up:half-page-up,alt-down:half-page-down'"
-
 export LC_ALL=zh_TW.UTF-8
 export PATH="$PATH:$BASEDIR/bat/bin:$HOME/.local/bin"
 
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+
 ### === aliases ===
 alias py="python"
-alias pfc="fzf | xargs code"
+
 
 ### === custom functions ===
+function pfc() {
+    local PRVIEW_WINDOW="--preview-window 'right:57%'"
+    local PREVIEW="--preview 'bat --color always --style=numbers --line-range :300 {}'"
+    local BIND="--bind 'ctrl-y:preview-up,ctrl-e:preview-down,ctrl-b:preview-page-up,ctrl-f:preview-page-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,shift-up:preview-top,shift-down:preview-bottom,alt-up:half-page-up,alt-down:half-page-down'"
+    local files
+    files=$(eval "fzf $PRVIEW_WINDOW $PREVIEW $BIND --exit-0") && code  $files
+}
+
 function pip() {
     if [[ $1 == "search" ]]; then
         command curl https://pypi.org/pypi/$2/json | jq .releases | jq 'keys[]'
@@ -48,3 +52,6 @@ function realpath() {
 function docker_rn() {
     docker rmi $1 $(docker images --filter "dangling=true" -q --no-trunc)
 }
+
+export JABBA_VERSION='0.11.2'
+[ -s "$JABBA_HOME/jabba.sh" ] && source "$JABBA_HOME/jabba.sh"
